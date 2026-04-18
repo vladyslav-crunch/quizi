d98c4577f71a24ae5a976e023003e5b4ca2e545a
vladyslav-crunch <vladyslav.crunch@gmail.com>
2026-04-18 23:26:44 +0200

    update build script and add nitro plugin to Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
 import { defineConfig } from 'vite'
 import { devtools } from '@tanstack/devtools-vite'
-
+import { nitro } from 'nitro/vite';
 import { tanstackStart } from '@tanstack/react-start/plugin/vite'
 
 import viteReact from '@vitejs/plugin-react'
@@ -8,7 +8,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 const config = defineConfig({
   resolve: { tsconfigPaths: true },
-  plugins: [devtools(), tailwindcss(), tanstackStart(), viteReact()],
+  plugins: [devtools(), tailwindcss(), tanstackStart(), nitro(), viteReact()],
 })
 
 export default config
